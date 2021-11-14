621890df4c210102dffa8e0ee4e6cc51579b6f5a
Ulyanov-programmer <ccoldatheinrich@yandex.ru>
2021-11-14 12:31:11 +0400

    first commit

---
+++ #src/scripts/script.js
@@ -0,0 +1,10 @@
+'use strict'
+
+let doc = document;
+let innerWindowWidth = () => window.innerWidth;
+let innerWindowHeight = () => window.innerHeight;
+
+
+// ? If you see an error here, it's normal.
+@@include('_fsNavmenu.js');
+@@include('_sliders.js');
