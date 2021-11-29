a66321e66d2d308b7ccd36871d1636c1469dccae
Ulyanov-programmer <ccoldatheinrich@yandex.ru>
2021-11-29 11:43:23 +0400

    Improved sidebar.

--- #src/scripts/script.js
+++ #src/scripts/script.js
@@ -8,13 +8,14 @@ let innerWindowHeight = () => window.innerHeight;
 // ? If you see an error here, it's normal.
 @@include('_fsNavmenu.js');
 @@include('_sliders.js');
-
+  
 
 function toggleSidebar(e) {
   sidebar.classList.toggle('active');
   toggleSidebarBtn.classList.toggle('active');
 }
 const toggleSidebarBtn = document.getElementById('sidebar-toggle-btn');
+
 const sidebar = document.querySelector('.sidebar');
 
 toggleSidebarBtn.addEventListener('click', toggleSidebar);
